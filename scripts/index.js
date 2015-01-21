8252f3e9e1e74f6fdd5f84acab26f1ab88c7bd37
musicpop <zp263@163.com>
2015-01-21 10:28:48 +0800

    1.find users  like nick_name 2.add search place_holder

--- scripts/index.js
+++ scripts/index.js
@@ -44,3 +44,24 @@ function getWindowParam(name){
    if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
       return decodeURIComponent(name[1]);
 }
+/**
+
+ * 替换字符串中的字段.
+
+ * @param {String} str 模版字符串
+
+ * @param {Object} o json data
+
+ * @param {RegExp} [regexp] 匹配字符串的正则表达式
+
+ */
+
+function substitute(str,o,regexp){
+
+  return  str.replace(regexp || /\\?\{([^{}]+)\}/g, function (match, name) {
+
+    return (o[name] === undefined) ? '' : o[name];
+
+  });
+
+}
