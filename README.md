e89731c9171dcc8abaaa277140d5f67c163d8ef2
musicpop <zp263@163.com>
2015-01-26 17:38:42 +0800

    delete some files

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-Weibo
-=====
-
-a demo using HTML/JS to show interface and basic function of weibo.
-
-Best on 1280 * 768.
+ 
--- scripts/yanzheng.js
+++ scripts/yanzheng.js
@@ -1,30 +1,5 @@
 // JavaScript Document
-
-function yanzheng(){
-	var nm = document.getElementById("myname").value;
-	var pw = document.getElementById("password").value; 
-	var kid = document.getElementById("nickname").value;
-	var ym = document.getElementById("check").value;
-	
-	if(null == nm||"" == nm || nm >16 || nm <6){
-		alert("用户名不合法！！");
-		return;
-		
-		}
-	if(null == pw ||""==pw||pw>12||pw<4){
-	alert("密码不合法！！");
-	return;
-	
-	}
-	
-	if(kid==""){alert("昵称不合法！！");return;}
-    if("cm2hq" != ym){
-		alert("验证码不正确！！");
-		return;
-		}
-	alert("数据已提交，注册成功！！");
-	
-	}
+ 
 
 	function check_mobile(mobile){
 
